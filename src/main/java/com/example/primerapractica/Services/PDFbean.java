d09fde2d095f5e05a088f1bfeb10acb0b82cb304
Darwin C <dc162005@gmail.com>
2024-10-01 16:38:29 -0500

    Avance de pdf

---
+++ src/main/java/com/example/primerapractica/Services/PDFbean.java
@@ -0,0 +1,69 @@
+package com.example.primerapractica.Services;
+
+import com.example.primerapractica.Models.Entity.Detalle;
+import com.example.primerapractica.Models.Entity.Encabezado;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.CMYKColor;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class PDFbean {
+
+    public void generate(Encabezado encabezado, List<Detalle> detalleList, HttpServletResponse response) throws DocumentException, IOException {
+        // Creating the Object of Document
+        Document document = new Document(PageSize.A4);
+        // Getting instance of PdfWriter
+        PdfWriter.getInstance(document, response.getOutputStream());
+        // Opening the created document to change it
+        document.open();
+        // Creating font
+        // Setting font style and size
+        Font fontTiltle = FontFactory.getFont(FontFactory.TIMES_ROMAN);
+        fontTiltle.setSize(20);
+        // Creating paragraph
+        Paragraph paragraph1 = new Paragraph("Factura Nº"+encabezado.getId(), fontTiltle);
+        // Aligning the paragraph in the document
+        paragraph1.setAlignment(Paragraph.ALIGN_CENTER);
+        // Adding the created paragraph in the document
+        document.add(paragraph1);
+        // Creating a table of the 4 columns
+        PdfPTable table = new PdfPTable(4);
+        // Setting width of the table, its columns and spacing
+        table.setWidthPercentage(100);
+        table.setWidths(new int[] {3,3,3,3});
+        table.setSpacingBefore(5);
+        // Create Table Cells for the table header
+        PdfPCell cell = new PdfPCell();
+        // Setting the background color and padding of the table cell
+        cell.setBackgroundColor(CMYKColor.BLUE);
+        cell.setPadding(5);
+        // Creating font
+        // Setting font style and size
+        Font font = FontFactory.getFont(FontFactory.TIMES_ROMAN);
+        font.setColor(CMYKColor.WHITE);
+        // Adding headings in the created table cell or  header
+        // Adding Cell to table
+        cell.setPhrase(new Phrase("ID", font));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Student Name", font));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Email", font));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Mobile No", font));
+        table.addCell(cell);
+        // Iterating the list of students
+
+        // Adding the created table to the document
+        document.add(table);
+        // Closing the document
+        document.close();
+    }
+}
+
